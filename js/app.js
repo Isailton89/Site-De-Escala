7834be98f7f9eac75a37769d7e039373d434da86
Isailton89 <113316233+Isailton89@users.noreply.github.com>
2023-06-21 19:31:16 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -114,7 +114,7 @@ function enviarEscala3() {
         Operadores: operadoresJson,
     };
 
-    const url = 'https://isailton.pythonanywhere.com/receber-dados';
+    const url = 'https://Isailton.pythonanywhere.com/receber-dados';
 
     fetch(url, {
         method: 'POST',
